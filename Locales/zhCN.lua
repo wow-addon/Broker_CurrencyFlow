bb5831a5137fd012986756528c71d73d047616f7
m42i <mfourtytwoi@gmail.com>
2012-04-25 22:36:06 +0200

    Added Simplified Chinese translation, thanks ttpod.

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -2,10 +2,118 @@
 Project                 : Broker_Currencyflow
 Description             : Chinese translation file (zhCN)
 Author                  : Aledara (wowi AT jocosoft DOT com)
-Translator              : ...
+Translator              : ttpod
 Revision                : $Rev: 36 $
 ********************************************************************* ]]
 
 local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale( "Currencyflow", "zhCN" )
 if not L then return end
+  
+L["CFG_COLORMAXREACHED"] = "着色已达上限的货币" -- Needs review
+L["CFG_CONFIRMDELETE"] = "你确定想要删除角色 \"%s\" 吗？" -- Needs review
+L["CFG_CONFIRMRESETSESSION"] = "你确定想要重置本次数据吗？" -- Needs review
+L["CFGDESC_BUTTONFIRST"] = "按钮上显示的第一种货币" -- Needs review
+L["CFGDESC_BUTTONFOURTH"] = "按钮上显示的第四种货币" -- Needs review
+L["CFGDESC_BUTTONSECOND"] = "按钮上显示的第二种货币" -- Needs review
+L["CFGDESC_BUTTONTHIRD"] = "按钮上显示的第三种货币" -- Needs review
+L["CFGDESC_CASHFORMAT"] = "选择货币的显示方式" -- Needs review
+L["CFGDESC_COLORMAXREACHED"] = "如果某种货币达到总量上限或周上限，则橙色显示。" -- Needs review
+L["CFGDESC_DELETECHAR"] = "选择想要删除的角色" -- Needs review
+L["CFGDESC_OTHERCHARS"] = "显示“角色”数据段" -- Needs review
+L["CFGDESC_SHOWCASHDETAIL"] = "显示获得/消费金币行" -- Needs review
+L["CFGDESC_SHOWCASHPERHOUR"] = "显示每小时金币列" -- Needs review
+L["CFGDESC_SHOWCOLUMNFOR"] = "显示 %s 数据列" -- Needs review
+L["CFGDESC_SHOWTOTALS"] = "显示本区中所有角色的总计数据" -- Needs review
+L["CFGDESC_SORTDESC"] = "如选中，将按照从高到低的顺序排列数据" -- Needs review
+L["CFGDESC_SORTOTHERCHARS"] = "选择角色列表的排序方式" -- Needs review
+L["CFGDESC_TTSCALE"] = "选择鼠标提示的大小" -- Needs review
+L["CFGHDR_ARCHFRAGMENTS"] = "考古碎片" -- Needs review
+L["CFGHDR_BUTTON"] = "按钮" -- Needs review
+L["CFGHDR_DELETECHAR"] = "删除角色" -- Needs review
+L["CFGHDR_GENERAL"] = "常规" -- Needs review
+L["CFGHDR_HISTORY"] = "历史部分" -- Needs review
+L["CFGHDR_OTHERCHARS"] = "本区其他角色" -- Needs review
+L["CFGHDR_OTHERCURRENCY"] = "其他货币" -- Needs review
+L["CFGHDR_PVE"] = "PVE货币" -- Needs review
+L["CFGHDR_PVP"] = "PvP货币" -- Needs review
+L["CFGHDR_TOOLTIP"] = "鼠标提示" -- Needs review
+L["CFGHDR_TOTALS"] = "总计" -- Needs review
+L["CFGNAME_BUTTONFIRST"] = "第一种货币" -- Needs review
+L["CFGNAME_BUTTONFOURTH"] = "第四种货币" -- Needs review
+L["CFGNAME_BUTTONSECOND"] = "第二种货币" -- Needs review
+L["CFGNAME_BUTTONTHIRD"] = "第三种货币" -- Needs review
+L["CFGNAME_CASHFORMAT"] = "货币格式" -- Needs review
+L["CFGNAME_CHARACTERS"] = "角色" -- Needs review
+L["CFGNAME_DELETE"] = "删除" -- Needs review
+L["CFGNAME_GAINED"] = "获得" -- Needs review
+L["CFGNAME_IGNORECHARS"] = "忽略角色" -- Needs review
+L["CFGNAME_MONTHSELF"] = "本月(自己)" -- Needs review
+L["CFGNAME_MONTHTOTAL"] = "本月(总计)" -- Needs review
+L["CFGNAME_OTHERCHARS"] = "显示其他角色" -- Needs review
+L["CFGNAME_PROFIT"] = "利润" -- Needs review
+L["CFGNAME_SHOWCASHDETAIL"] = "显示金币细节" -- Needs review
+L["CFGNAME_SHOWCASHPERHOUR"] = "显示每小时金币" -- Needs review
+L["CFGNAME_SHOWTOTALS"] = "显示总数" -- Needs review
+L["CFGNAME_SORTDESC"] = "降序排列" -- Needs review
+L["CFGNAME_SORTOTHERCHARS"] = "角色排序方式" -- Needs review
+L["CFGNAME_SPENT"] = "花费" -- Needs review
+L["CFGNAME_THISSESSION"] = "本次" -- Needs review
+L["CFGNAME_TIPOPTIONS"] = "右键点击打开选项窗口" -- Needs review
+L["CFGNAME_TIPRESETSESSION"] = "Shift+点击重置本次在线数据" -- Needs review
+L["CFGNAME_TODAYSELF"] = "今天(自己)" -- Needs review
+L["CFGNAME_TODAYTOTAL"] = "今天(总数)" -- Needs review
+L["CFGNAME_TOTAL"] = "总计" -- Needs review
+L["CFGNAME_TTSCALE"] = "鼠标提示缩放" -- Needs review
+L["CFGNAME_WEEKSELF"] = "本周(自己)" -- Needs review
+L["CFGNAME_WEEKTOTAL"] = "本周(总数)" -- Needs review
+L["CFGNAME_YESTERDAYSELF"] = "昨天(自己)" -- Needs review
+L["CFGNAME_YESTERDAYTOTAL"] = "昨天(总数)" -- Needs review
+L["CFGOPT_BTNMONEY"] = "当前金币" -- Needs review
+L["CFGOPT_BTNMONTHPERHOUR"] = "本月 金/小时" -- Needs review
+L["CFGOPT_BTNMONTHTOTAL"] = "本月金币总数" -- Needs review
+L["CFGOPT_BTNNONE"] = "无" -- Needs review
+L["CFGOPT_BTNOTHER"] = "%s" -- Needs review
+L["CFGOPT_BTNSESSIONPERHOUR"] = "本次 金/小时" -- Needs review
+L["CFGOPT_BTNSESSIONTOTAL"] = "本次金币总数" -- Needs review
+L["CFGOPT_BTNTODAYPERHOUR"] = "今天 金/小时" -- Needs review
+L["CFGOPT_BTNTODAYTOTAL"] = "今天金币总数" -- Needs review
+L["CFGOPT_BTNWEEKPERHOUR"] = "本周 金/小时" -- Needs review
+L["CFGOPT_BTNWEEKTOTAL"] = "本周金币总数" -- Needs review
+L["CFGOPT_CF_COINS"] = "完整(图标)" -- Needs review
+L["CFGOPT_CF_CONDENSED"] = "简略(无单位)" -- Needs review
+L["CFGOPT_CF_FULL"] = "完整(文字)" -- Needs review
+L["CFGOPT_CF_SHORT"] = "缩略(x.x金)" -- Needs review
+L["CFGOPT_SORTNAME"] = "名称" -- Needs review
+L["CFGPAGE_CHARACTERS"] = "角色" -- Needs review
+L["CFGPAGE_COLUMNS"] = "数据列" -- Needs review
+L["CFGPAGE_PROFILES"] = "配置" -- Needs review
+L["CFGPAGE_SECTIONS"] = "数据段" -- Needs review
+L["CFGTXT_DELETECHAR"] = "删除一个角色后，角色所有的历史数据都将被移除。" -- Needs review
+L["CFGTXT_IGNOREDCHARS"] = "被忽略的角色将会被完全排除在本插件之外。这些角色无历史数据，不会显示在鼠标提示中，也不会包含在任何统计计算内。" -- Needs review
+L["NAME_AF_DRAENEI"] = "德莱尼" -- Needs review
+L["NAME_AF_DWARF"] = "矮人" -- Needs review
+L["NAME_AF_FOSSIL"] = "化石" -- Needs review
+L["NAME_AF_NERUBIAN"] = "蛛魔" -- Needs review
+L["NAME_AF_NIGHTELF"] = "暗夜精灵" -- Needs review
+L["NAME_AF_ORC"] = "兽人" -- Needs review
+L["NAME_AF_OTHER"] = "其他" -- Needs review
+L["NAME_AF_TOLVIR"] = "托维尔" -- Needs review
+L["NAME_AF_TROLL"] = "巨魔" -- Needs review
+L["NAME_AF_VRYKULL"] = "维库人" -- Needs review
+L["NAME_CHAMPIONSEAL"] = "冠军的徽记" -- Needs review
+L["NAME_CHEFSAWARD"] = "大厨的奖牌" -- Needs review
+L["NAME_CONQUESTPOINTS"] = "征服点数" -- Needs review
+L["NAME_DALCOOKINGAWARD"] = "达拉然烹饪奖牌" -- Needs review
+L["NAME_DALJCTOKEN"] = "达拉然珠宝匠硬币" -- Needs review
+L["NAME_ESSENCEDEATHWING"] = "死亡之翼的堕落精华" -- Needs review
+L["NAME_HONORPOINTS"] = "荣誉点数" -- Needs review
+L["NAME_ILLJCTOKEN"] = "珠宝名匠的荣誉奖章" -- Needs review
+L["NAME_JUSTICEPOINTS"] = "正义点数" -- Needs review
+L["NAME_MARKOFTHEWORLDTREE"] = "世界之树的印记" -- Needs review
+L["NAME_MONEY"] = "金币" -- Needs review
+L["NAME_MOTEDARKNESS"] = "暗月奖券" -- Needs review
+L["NAME_NO"] = "否" -- Needs review
+L["NAME_TOLBARADCOMMENDATION"] = "托尔巴拉德奖章" -- Needs review
+L["NAME_VALORPOINTS"] = "勇气点数" -- Needs review
+L["NAME_YES"] = "是" -- Needs review
 
