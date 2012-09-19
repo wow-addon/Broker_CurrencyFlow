f5fa21eaae7c8c5706f9111a8724b24b7048237e
m42i <mfourtytwoi@gmail.com>
2012-09-19 16:29:46 +0200

    Add automated zhTW translation.

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -2,10 +2,11 @@
 Project                 : Broker_Currencyflow
 Description             : Chinese (Traditional) translation file (zhTW)
 Author                  : Aledara (wowi AT jocosoft DOT com)
-Translator              : ...
+Translator              : see http://www.wowace.com/addons/broker_currencyflow/localization
 Revision                : $Rev: 36 $
 ********************************************************************* ]]
 
 local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale( "Currencyflow", "zhTW" )
 if not L then return end
 
+--@localization(locale="zhTW", format="lua_additive_table")@
